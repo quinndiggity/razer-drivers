2e11c8e25db621a7d8d900c57291bfdc68124e84
Terry Cain <terry@terrys-home.co.uk>
2017-03-10 00:18:30 +0000

    test3

--- driver/razerchromacommon.c
+++ driver/razerchromacommon.c
@@ -467,7 +467,7 @@ struct razer_report razer_chroma_standard_matrix_effect_custom_frame(unsigned ch
 struct razer_report razer_chroma_standard_matrix_set_custom_frame(unsigned char row_index, unsigned char start_col, unsigned char stop_col, unsigned char *rgb_data)
 {
     size_t row_length = (size_t) (((stop_col + 1) - start_col) * 3);
-    struct razer_report report = get_razer_report(0x03, 0x0B, 0x46); // In theory should be able to leave data size at max as we have start/stop
+    struct razer_report report = get_razer_report(0x03, 0x0B, 0x34); // In theory should be able to leave data size at max as we have start/stop
     int index = 4 + (start_col * 3);
     
     // printk(KERN_ALERT "razerkbd: Row ID: %d, Start: %d, Stop: %d, row length: %d\n", row_index, start_col, stop_col, (unsigned char)row_length);
